0862e24af2eff35479db0879d86726d25811cfd6
John Crepezzi <john.crepezzi@gmail.com>
2012-03-12 22:05:21 -0400

    Autoload AttributeError

--- lib/cereal_eyes.rb
+++ lib/cereal_eyes.rb
@@ -2,4 +2,6 @@ module CerealEyes
 
   autoload :Document, File.dirname(__FILE__) + '/cereal_eyes/document'
 
+  autoload :AttributeError, File.dirname(__FILE__) + '/cereal_eyes/attribute_error'
+
 end
